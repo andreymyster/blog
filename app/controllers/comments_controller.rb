3b47c8c3df8fbf0917a7ebb3831746cccfe399fe
andreymyster <and001@gmail.com>
2016-03-16 18:33:10 +0300

    Current username at save comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,19 +4,15 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    # @article.comments.create(comment_params)
-
-    # art = Article.new
-    @article.comments.author = current_user.username
-    @article.comments.body = params.require(:comment).permit(:body)
-    @article.save
-
+    @c = @article.comments.new(comment_params)
+    @c.author = current_user.username
+    @c.save
     redirect_to article_path(@article)
   end
 
-  # private
+  private
 
-  # def comment_params
-  #   params.require(:comment).permit(:body)
-  # end
+  def comment_params
+    params.require(:comment).permit(:author, :body)
+  end
 end
