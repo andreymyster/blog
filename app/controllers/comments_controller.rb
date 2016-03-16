83fd2c5d589e97c11303a0f1aa5fb1417a8b29d9
andreymyster <and001@gmail.com>
2016-03-16 17:04:27 +0300

    Menu at layout (bootstrap)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,19 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+    # @article.comments.create(comment_params)
+
+    # art = Article.new
+    @article.comments.author = current_user.username
+    @article.comments.body = params.require(:comment).permit(:body)
+    @article.save
 
     redirect_to article_path(@article)
   end
 
-  private
+  # private
 
-  def comment_params
-    params.require(:comment).permit(:author, :body)
-  end
+  # def comment_params
+  #   params.require(:comment).permit(:body)
+  # end
 end
