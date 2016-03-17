bbcaceef2017066bbefe095adc92c4c17cdc74b0
andreymyster <and001@gmail.com>
2016-03-17 22:17:57 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,5 +10,15 @@ describe Article do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
   end
-  
+
+  describe '#subject' do
+    it "returns the article title" do
+      # создаем объект хитрым способом
+      article = create(:article, title: 'Lorem')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem'
+    end
+  end
+
 end
