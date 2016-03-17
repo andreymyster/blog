cbc096659b3b4f75644cf0e3927d3614817e6af2
andreymyster <and001@gmail.com>
2016-03-17 19:58:08 +0300

    Article model separate spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe Article do
-  it { should have_many :comments }
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :text }
+
+  describe 'Associations' do
+    it { should have_many :comments }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+  
 end
