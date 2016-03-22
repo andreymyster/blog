938a6f07402458e27209ad3873c92c8e1d7186e2
andreymyster <and001@gmail.com>
2016-03-22 19:51:01 +0300

    Update Comment model spec - max length of body is 4000

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe Comment do
   it { should belong_to :article }
+
+  it { should allow_value(qwerty_string(4000)).for(:body) }
+  it { should_not allow_value(qwerty_string(4001)).for(:body) }
 end
