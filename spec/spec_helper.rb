214263a6f0dcd66806772826708effbde157f62f
andreymyster <and001@gmail.com>
2016-03-22 19:33:40 +0300

    Add helper func for rspec - qwerty-string

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,14 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+# функция создания строки нужной длины
+def qwerty_string num
+  str = ''
+  num.times do
+    str += 'a'
+  end
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
